468d5cd077228d8eeb4041c89e8aa926ed3a297a
Elliot Sturzaker <elliotsturzaker@Elliots-MacBook-Pro.local>
2024-07-19 13:31:29 +1200

    test deploy

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,13 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook:
+        "https://api.vercel.com/v1/integrations/deploy/prj_lZc69jFp381lGp3F3JhUaZM9nUPq/rXoXjrQsGB",
+      apiToken: "udt0iKmoiKCL5n5keJcolUis",
+      appFilter: "wild-api",
+      teamFilter: "team_iXsy6sqoarYXVk11tJu3Mf9n",
+      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+    },
+  },
+});
